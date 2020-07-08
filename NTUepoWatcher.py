a8f57739c570469adf76ae648da4a256d765e772
tauChang <44998547+tauChang@users.noreply.github.com>
2020-07-08 11:03:55 +0800

    Update NTUepoWatcher.py
    Modularize

--- NTUepoWatcher.py
+++ NTUepoWatcher.py
@@ -1,74 +1,83 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+from datetime import datetime
 import hashlib
 import smtplib, ssl
+from getpass import getpass
 
-epoUserName = 'xxx'
-epoPassword = 'yyy'
-senderEmail = 'zzz@gmail.com'
-senderPassword = 'vvv'
-receiverEmail = senderEmail
-timeInterval = 10
-
-
-payload = {'user':epoUserName, 'pass':epoPassword}
-loginURL = 'https://if163.aca.ntu.edu.tw/eportfolio/login.asp'
-toPostURL = 'https://web2.cc.ntu.edu.tw/p/s/login2/p1.php'
-gradeSectionURL = 'https://if163.aca.ntu.edu.tw/eportfolio/student/CourseSem.asp'
-
-port = 587  # For starttls
-smtpServer = 'smtp.gmail.com'
-message = """\
-Subject: Something has changed!
-
-Something has changed on NTUepo! Check it out."""
-
-
-# create session
-session= requests.session()
-
-# access log in page to obtain cookie
-session.get(loginURL)
-
-# log in with username and password
-session.post(toPostURL, payload)
-
-# access the grade page and hash
-response = session.get(gradeSectionURL)
-oldHash = hashlib.sha224(response.content).hexdigest()
-
-
-while True:
-    try:
-        time.sleep(10)
-        response = session.get(gradeSectionURL)
-        newHash = hashlib.sha224(response.content).hexdigest()
-        if newHash == oldHash:
-            print('nothing changed...')
-            oldHash = newHash
-            continue
-        else:
-            print('DAMN SOMETHING CHANGED!! BRO')
-            break
-            context = ssl.create_default_context()
-            with smtplib.SMTP(smtpServer, port) as server:
-                server.ehlo()  # Can be omitted
-                server.starttls(context=context)
-                server.ehlo()  # Can be omitted
-                server.login(senderEmail, senderPassword)
-                server.sendmail(senderEmail, receiverEmail, message)
-            oldHash = newHash
-    except (EOFError, KeyboardInterrupt):
-        print('ctrl c pressed')
-        break
-    except:
-        print('fuck something wrong')
-        break
+class NTUepoWatcher():
+    loginURL = 'https://if163.aca.ntu.edu.tw/eportfolio/login.asp'
+    toPostURL = 'https://web2.cc.ntu.edu.tw/p/s/login2/p1.php'
+    gradePageURL = 'https://if163.aca.ntu.edu.tw/eportfolio/student/CourseSem.asp'
+    session = requests.session()
     
+    def __init__(self, epoUserName, epoPassword, senderEmail, senderPassword, receiverEmail, timeInterval):
+        self.epoUserName = epoUserName
+        self.epoPassword = epoPassword
+        self.senderEmail = senderEmail
+        self.senderPassword = senderPassword
+        self.receiverEmail = receiverEmail
+        self.timeInterval = timeInterval
+        
+    def createLoginPayload(self):
+        return {'user':self.epoUserName, 'pass':self.epoPassword}
+        
+    def makeMessage(self, oldContent, newContent):
+        message = """\
+        Subject: Something has changed!
 
-
-#soup = BeautifulSoup(result.content.decode('Big5'), 'html.parser')
-#print(soup.prettify())
-
-
+        Something has changed on NTUepo! Check it out."""
+        return message
+        
+        
+    def sendEmail(self, message):
+        context = ssl.create_default_context()
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.ehlo()  # Can be omitted
+            server.starttls(context=context)
+            server.ehlo()  # Can be omitted
+            server.login(self.senderEmail, self.senderPassword)
+            server.sendmail(self.senderEmail, self.receiverEmail, message)
+            
+    def login(self):
+        self.session.get(self.loginURL)
+        self.session.post(self.toPostURL, self.createLoginPayload())
+        
+    def getGradePage(self):
+        return self.session.get(self.gradePageURL)
+        
+    def getTime(self):
+        return "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]"
+    
+    def run(self):
+        self.login()
+        oldContent = self.getGradePage().content
+        while True:
+            try:
+                time.sleep(self.timeInterval)
+                newContent = self.getGradePage().content
+                #soup = BeautifulSoup(newContent.decode('Big5'), 'html.parser')
+                #print(soup.prettify())
+                if oldContent == newContent:
+                    print(self.getTime() + " nothing changed...")
+                    oldContent = newContent
+                else:
+                    print(self.getTime() + " something has changed!")
+                    self.sendEmail(self.makeMessage(oldContent, newContent))
+                    oldContent = newContent
+            except (EOFError, KeyboardInterrupt):
+                print('Keyboard Interrupt\n')
+                break
+            except Exception as e:
+                print(e)
+                break
+                    
+def main():
+    epoPassword = getpass('NTUepo password: ')
+    gmailPassword = getpass('Gmail password: ')
+    watcher = NTUepoWatcher('epoUsername', epoPassword, 'gmailSender', gmailPassword, 'gmailReceiver', 600)
+    watcher.run()
+    
+if __name__ == '__main__':
+    main()
